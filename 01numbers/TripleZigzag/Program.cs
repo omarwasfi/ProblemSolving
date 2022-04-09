6e5214fe5f13eadb328d225f32804ef55c4aee0e
Omar Wasfi <contact@omarwasfi.com>
2022-04-09 16:26:44 +0200

    TripleZigzag

---
+++ 01numbers/TripleZigzag/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] nums = new int[] { 1, 2, 1, 3, 4 };
+            var resuls = solution(nums);
+        }
+
+        static int[] solution(int[] numbers)
+        {
+
+            List<int> results = new List<int>();
+
+            int currentIndex = 0;
+
+            while(currentIndex + 2 <= numbers.Length - 1)
+            {
+                if (numbers[currentIndex] < numbers[currentIndex + 1] && numbers[currentIndex + 1] > numbers[currentIndex + 2])
+                {
+                    results.Add(1);
+                }
+
+                else if (numbers[currentIndex] > numbers[currentIndex + 1] && numbers[currentIndex + 1] < numbers[currentIndex + 2])
+                {
+                    results.Add(1);
+                }
+                else
+                {
+                    results.Add(0);
+
+                }
+
+                currentIndex++;
+            }
+
+
+            return results.ToArray();
+
+        }
+
+        
+    }
+
+
+
+}
