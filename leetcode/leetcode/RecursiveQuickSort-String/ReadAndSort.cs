3337459842d6fdd0a13bb6825ca022bd2dfd275a
Omar Wasfi <contact@omarwasfi.com>
2022-06-04 15:18:00 +0200

    Binary tree depthFirst printing

---
+++ leetcode/leetcode/RecursiveQuickSort-String/ReadAndSort.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace RecursiveQuickSort_String
+{
+	public class ReadAndSort
+	{
+        public List<string> QuickSort(List<string> Names)
+        {
+            if (Names.Count <= 1)
+                return Names;
+
+            Random random = new Random();
+
+            int pivot = random.Next(0, Names.Count - 1);
+
+            List<string> LesserSide = new List<string>();
+            List<string> GreaterSide = new List<string>();
+
+            for (int i = 0; i < Names.Count; i++)
+            {
+                if (i != pivot)
+                {
+                    if (Names[i].CompareTo(Names[pivot]) <= 0)
+                    {
+                        LesserSide.Add(Names[i]);
+                    }
+                    else
+                    {
+                        GreaterSide.Add(Names[i]);
+
+                    }
+                }
+            }
+
+
+            LesserSide = QuickSort(LesserSide);
+            GreaterSide = QuickSort(GreaterSide);
+
+
+            List<string> sortedNames = new List<string>();
+
+            sortedNames.AddRange(LesserSide);
+            sortedNames.Add(Names[pivot]);
+            sortedNames.AddRange(GreaterSide);
+
+            return sortedNames;
+
+        }
+
+        public List<string> ReadListOfStrings(string path)
+        {
+            string[] readText = File.ReadAllLines(path);
+
+            return readText.ToList();
+        }
+    }
+}
+
---
+++ leetcode/leetcode/Testing-XUnit/RecursiveQuickSortString.cs
@@ -0,0 +1,34 @@
+﻿namespace Testing_XUnit;
+
+using RecursiveQuickSort_String;
+
+public class RecursiveQuickSortString
+{
+    [Fact]
+    public void ReadStrings()
+    {
+        ReadAndSort readAndSort = new ReadAndSort();
+
+        Assert.Equal(readAndSort.ReadListOfStrings("/Users/omarwasfi/Documents/GitHub/ProblemSolving/leetcode/leetcode/RecursiveQuickSort-String/1000strings.txt").Count, 1000);
+    }
+
+    [Fact]
+    public void ReadAndSortStrings()
+    {
+        ReadAndSort readAndSort = new ReadAndSort();
+
+        List<string> strings = readAndSort.ReadListOfStrings("/Users/omarwasfi/Documents/GitHub/ProblemSolving/leetcode/leetcode/RecursiveQuickSort-String/1000strings.txt");
+
+        List<string> stringsSorted = readAndSort.QuickSort(strings);
+
+        List<string> expectedSorting = strings;
+        expectedSorting.Sort();
+
+
+        Assert.Equal(strings.Count, 1000);
+
+        Assert.Equal(stringsSorted, expectedSorting);
+
+    }
+
+}
