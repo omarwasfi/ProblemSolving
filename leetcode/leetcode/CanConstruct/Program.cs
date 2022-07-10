7e41778a960159b8a802b57b5bdeb606f1074f8c
Omar Wasfi <contact@omarwasfi.com>
2022-07-10 06:53:55 +0200

    Fixing + adding HowManyCanConstruct

--- leetcode/leetcode/CanConstruct/Program.cs
+++ leetcode/leetcode/CanConstruct/Program.cs
@@ -8,7 +8,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
 
            
-            Console.WriteLine(CanConstruct("skateboardww", new List<string>() { "w" ,"sk" , "ate","bo" , "ard" }));
+            Console.WriteLine(CanConstruct("skateboardwasa", new List<string>() { "w" , "wa" , "sa" , "a" ,"sk" , "bo" , "ard" ,"ate" }));
+
+
+            Console.WriteLine(HowManyCanConstruct("skateboardwasaaaaaaaaaaaaaaaaaaaaaaaaaaaa", new List<string>() { "w", "wa", "sa", "a", "sk", "bo", "ard", "ate" }));
 
 
 
@@ -18,7 +21,9 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             if (TargetWord.Count() == 0) return true;
 
-            foreach(string word in Source)
+            bool currentState = false;
+
+            foreach (string word in Source)
             {
                 // check if the word at the begining of the targetWord
                 // get the new word call CanConstruct Again
@@ -32,7 +37,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
                     if (firstPartOfTheWord == word)
                     {
                         string newTarget = TargetWord.Substring(lenght, TargetWord.Count() - lenght);
-                        return CanConstruct(newTarget, Source);
+                        Console.WriteLine(newTarget);
+                        currentState = CanConstruct(newTarget, Source);
 
                     }
 
@@ -42,18 +48,82 @@ namespace MyApp // Note: actual namespace depends on the project name.
                     {
 
                         string newTarget = TargetWord.Substring(0, TargetWord.Count() - lenght);
-                        return CanConstruct(newTarget, Source);
+                        Console.WriteLine(newTarget);
+
+                        currentState = CanConstruct(newTarget, Source);
 
                     }
                 }
+                else
+                {
+
+                }
 
                
-                
+                if(currentState == true)
+                {
+                    return currentState;
+                }
 
             }
 
 
-            return false;
+            return currentState;
+        }
+
+        public static int HowManyCanConstruct(string TargetWord, List<string> Source, Dictionary<int, List<int>> Memo = null)
+        {
+            if (TargetWord.Count() == 0) return 1;
+
+            int trueStateCounter = 0;
+
+            foreach (string word in Source)
+            {
+                // check if the word at the begining of the targetWord
+                // get the new word call CanConstruct Again
+                // check if the word at the end if the targetWord
+
+                int lenght = word.Count();
+
+                if (lenght > TargetWord.Count() != true)
+                {
+                    string firstPartOfTheWord = TargetWord.Substring(0, lenght);
+                    if (firstPartOfTheWord == word)
+                    {
+                        string newTarget = TargetWord.Substring(lenght, TargetWord.Count() - lenght);
+                        Console.WriteLine(newTarget);
+
+                        int result = HowManyCanConstruct(newTarget, Source);
+                        if (result > 0)
+                            trueStateCounter += result;
+
+                    }
+
+                    string lastPartOfTheWord = TargetWord.Substring(TargetWord.Count() - lenght, lenght);
+
+                    if (lastPartOfTheWord == word)
+                    {
+
+                        string newTarget = TargetWord.Substring(0, TargetWord.Count() - lenght);
+                        Console.WriteLine(newTarget);
+
+                        int result = HowManyCanConstruct(newTarget, Source);
+                        if (result > 0)
+                            trueStateCounter += result;
+                    }
+                }
+                else
+                {
+
+                }
+
+
+               
+
+            }
+
+
+            return trueStateCounter;
         }
     }
 
